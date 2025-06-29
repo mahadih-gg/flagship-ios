3695ebfe24b304c802210c255adf567030af7a65
mahadih <mahadih.dev@gmail.com>
2025-06-29 17:59:39 +0600

    Initialize Project

---
+++ src/components/FlagshipProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { FlagshipContainer } from "@thinkflagship/web-shorts";
+import { ReactNode } from "react";
+
+interface FlagshipProviderProps {
+  children: ReactNode;
+}
+
+export default function FlagshipProvider({ children }: FlagshipProviderProps) {
+
+  const licenseKey = process.env.NEXT_PUBLIC_FLAGSHIP_WEB_SDK_LICENSE_KEY;
+  return (
+    <FlagshipContainer licenseKey={licenseKey} platformId="https://flagship-web-sdk.vercel.app">
+      {children}
+    </FlagshipContainer>
+  );
+} 
